fcad60f34df840059fad0433a8c4bb45722c61b3
Gabe571 <gabriel.castro1028@gmail.com>
2021-04-20 09:40:59 -0700

    initial layout

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3> Reminders </h3>
+    < br />
+    <h3> New Reminders </h3>
+    <form>
+      <label htmlFor='id'> ID </label>
+      <br />
+      <input name='id' placeholder='id'></input>
+      <br />
+      <label  htmlFor='reminder'> Reminder </label>
+      <br />
+      <input name='reminder'placeholder='reminder'></input>  
+      <br />
+      <label htmlFor='time'> Time </label>
+      <br />
+      <input name='time' placeholder='time'></input>
+      < br />
+      <button type='submit'> Add </button>
+    </form>
     </div>
   );
 }
