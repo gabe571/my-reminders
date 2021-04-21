9e885c12054b879cbfb3c7286f4189e3b4be7442
Gabe571 <gabriel.castro1028@gmail.com>
2021-04-21 12:48:15 -0700

    app.js reminder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Listen to Podcast'},
+    { text: 'Watch Youtube video on React'},
+    { text: 'Build Reminder App'}
+  ]);
+
   return (
-    <div className="App">
-    <h3> Reminders </h3>
-    < br />
-    <h3> New Reminders </h3>
-    <form>
-      <label htmlFor='id'> ID </label>
-      <br />
-      <input name='id' placeholder='id'></input>
-      <br />
-      <label  htmlFor='reminder'> Reminder </label>
-      <br />
-      <input name='reminder'placeholder='reminder'></input>  
-      <br />
-      <label htmlFor='time'> Time </label>
-      <br />
-      <input name='time' placeholder='time'></input>
-      < br />
-      <button type='submit'> Add </button>
-    </form>
+    <div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
